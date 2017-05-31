5d79364caf9718c155a98e070124fed89596909a
Yudzeen <eugenejavinas@gmail.com>
2017-05-31 21:49:21 +0800

    Updated clear color

--- core/src/eug/awesome/yujin/YujinGame.java
+++ core/src/eug/awesome/yujin/YujinGame.java
@@ -3,6 +3,7 @@ package eug.awesome.yujin;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 
 import eug.awesome.yujin.screens.MainScreen;
 
@@ -14,5 +15,10 @@ public class YujinGame extends Game {
 		setScreen(new MainScreen(this));
 	}
 
-
+	@Override
+	public void render() {
+		Gdx.gl.glClearColor(0,0,0,1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		super.render();
+	}
 }
