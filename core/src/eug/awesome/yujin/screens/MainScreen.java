de91ecce5dc912120a7517d6a613f87d7c281423
Yudzeen <eugenejavinas@gmail.com>
2017-05-31 21:53:54 +0800

    Added tiled map

--- core/src/eug/awesome/yujin/screens/MainScreen.java
+++ core/src/eug/awesome/yujin/screens/MainScreen.java
@@ -1,5 +1,11 @@
 package eug.awesome.yujin.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
 import eug.awesome.yujin.YujinGame;
 import eug.awesome.yujin.player.Player;
 
@@ -13,16 +19,35 @@ public class MainScreen extends GameScreen {
 
     private Player player;
 
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer tiledMapRenderer;
+
     public MainScreen(YujinGame game) {
         super(game);
+        init();
+    }
+
+    private void init() {
+        map = new TmxMapLoader().load("tilemaps/home.tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(map);
+
         player = new Player();
-        player.setPosition(400, 200);
         stage.addActor(player);
+        stage.addListener(player.getPlayerController());
     }
 
     @Override
     public void render(float delta) {
         camera.position.set(player.getX(), player.getY(), 0);
+        tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render();
         super.render(delta);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        map.dispose();
+        tiledMapRenderer.dispose();
     }
 }
