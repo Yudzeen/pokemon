ba95ee386a0ad6e2fe41993fc620e810510b5024
Yudzeen <eugenejavinas@gmail.com>
2017-05-31 21:50:01 +0800

    Added tile size

--- core/src/eug/awesome/yujin/utlis/GameConstants.java
+++ core/src/eug/awesome/yujin/utlis/GameConstants.java
@@ -11,4 +11,6 @@ public class GameConstants {
     public static final int SCREEN_WIDTH = Gdx.graphics.getWidth();
     public static final int SCREEN_HEIGHT = Gdx.graphics.getHeight();
 
+    public static final int TILE_SIZE = 16;
+
 }
